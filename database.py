66384a7991aa840d62f1a9384715a9ca0f93c7d8
Jonathan Laurent <jonathanlaurent@Jonathans-MacBook-Air.local>
2023-09-11 20:17:23 -0700

    progressing

--- database.py
+++ database.py
@@ -16,42 +16,45 @@ engine = create_engine(
 })
 
 
-def load_job_from_db(id):
+def load_job_from_db():
   with engine.connect() as conn:
     result = conn.execute(
-      text("select * from jobs where id = " + id),
+      text("select * from applications"),
     )
     rows = result.all()
+    # print(rows)
+    myRow = rows[0]._mapping
     if len(rows) == 0:
       return None
     else:
-      return dict(rows[0]._mapping)
-    
-def add_application_to_db(job_id, data):
-  with engine.connect() as conn:
-    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education," +  
-                 " work_experience, resume_url) VALUES(:job_id, :full_name, :email, :linkedin_url, " + 
-                 ":education, :work_experience, :resume_url)")
+      print(myRow['song_artist'])
 
-    values={'job_id': job_id,
-            'full_name':data['full_name'],
-            'email':data['email'],
-            'linkedin_url':data['linkedin_url'],
-            'education':data['education'],
-            'work_experience':data['work_experience'],
-            'resume_url':data['resume_url']
-            }
-    # for some reason I have to use a dictionary ad Values and stil create all those variables in the query stmt
-    # don't know why but i'll have to do this for now lol
-    conn.execute(
-      query,
-      values
-      # job_id=job_id,
-      # full_name=data['full_name'],
-      # linkedin_url=data['linkedin_url'],
-      # email=data['email'],
-      # education=data['education'],
-      # work_experience=data['work_experience'],
-      # resume_url=data['resume_url']
-    )
+load_job_from_db()
+# def add_application_to_db(job_id, data):
+#   with engine.connect() as conn:
+#     query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education," +  
+#                  " work_experience, resume_url) VALUES(:job_id, :full_name, :email, :linkedin_url, " + 
+#                  ":education, :work_experience, :resume_url)")
+
+#     values={'job_id': job_id,
+#             'full_name':data['full_name'],
+#             'email':data['email'],
+#             'linkedin_url':data['linkedin_url'],
+#             'education':data['education'],
+#             'work_experience':data['work_experience'],
+#             'resume_url':data['resume_url']
+#             }
+#     # for some reason I have to use a dictionary ad Values and stil create all those variables in the query stmt
+#     # don't know why but i'll have to do this for now lol
+#     conn.execute(
+#       query,
+#       values
+#       # job_id=job_id,
+#       # full_name=data['full_name'],
+#       # linkedin_url=data['linkedin_url'],
+#       # email=data['email'],
+#       # education=data['education'],
+#       # work_experience=data['work_experience'],
+#       # resume_url=data['resume_url']
+#     )
   
